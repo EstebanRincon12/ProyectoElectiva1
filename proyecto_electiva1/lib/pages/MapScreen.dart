e35c6909e2e70f8a1462d2a45dac57a39e14f0a6
EstebanRincon12 <esteban.rincon@uptc.edu.co>
2024-10-14 15:38:45 -0500

    Adicion favoritos

--- proyecto_electiva1/lib/pages/MapScreen.dart
+++ proyecto_electiva1/lib/pages/MapScreen.dart
@@ -1,5 +1,6 @@
-// mapscreen.dart
 import 'package:flutter/material.dart';
+import 'package:flutter_speed_dial/flutter_speed_dial.dart';
+import 'package:proyecto_electiva1/pages/add_favorite_screen.dart';
 import 'package:proyecto_electiva1/widgets/destinations.dart';
 
 class MapScreen extends StatelessWidget {
@@ -10,16 +11,12 @@ class MapScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Colors.black,
-        title: const Text(
-          'A donde vamos?',
-          style: TextStyle(color: Colors.yellow),
-        ),
+        title: const _SearchBar(),
         centerTitle: true,
         elevation: 0,
       ),
       body: Column(
         children: [
-          // Mapa placeholder
           Expanded(
             child: Container(
               color: Colors.black,
@@ -31,9 +28,97 @@ class MapScreen extends StatelessWidget {
               ),
             ),
           ),
-          // Lista de ubicaciones y botón
           const Destinations(),
         ],
+      ),
+      floatingActionButton: SpeedDial(
+        animatedIcon: AnimatedIcons.menu_close,
+        backgroundColor: Colors.black,
+        children: [
+          SpeedDialChild(
+            child: const Icon(Icons.favorite, color: Colors.white),
+            label: 'Añadir a Favoritos',
+            backgroundColor: Colors.blue,
+            onTap: () {
+              Navigator.push(
+                context,
+                MaterialPageRoute(
+                  builder: (context) => const AddToFavoritesScreen(),
+                ),
+              );
+            },
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.directions_walk, color: Colors.white),
+            label: 'Comenzar Recorrido',
+            backgroundColor: Colors.green,
+            onTap: () {
+              print('Comenzar Recorrido');
+            },
+          ),
+          SpeedDialChild(
+            child: const Icon(Icons.favorite_border, color: Colors.white),
+            label: 'Eliminar de Favoritos',
+            backgroundColor: Colors.red,
+            onTap: () {
+              print('Eliminar de Favoritos');
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class _SearchBar extends StatefulWidget {
+  const _SearchBar({Key? key}) : super(key: key);
+
+  @override
+  _SearchBarState createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<_SearchBar> {
+  final FocusNode _focusNode = FocusNode();
+  final TextEditingController _controller = TextEditingController();
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _focusNode.dispose();
+    _controller.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {
+        FocusScope.of(context).requestFocus(_focusNode);
+      },
+      child: Container(
+        decoration: BoxDecoration(
+          color: Colors.white,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        child: TextField(
+          controller: _controller,
+          focusNode: _focusNode,
+          keyboardType: TextInputType.text,
+          textInputAction: TextInputAction.search,
+          decoration: const InputDecoration(
+            hintText: 'Buscar destino...',
+            prefixIcon: Icon(Icons.search, color: Colors.black),
+            border: InputBorder.none,
+            contentPadding: EdgeInsets.all(10),
+          ),
+          onSubmitted: (value) {
+            print("Buscar: $value");
+          },
+        ),
       ),
     );
   }
